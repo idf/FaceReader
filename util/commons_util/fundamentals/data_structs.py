8239190407a4ee7c01dbc438fc647860b165f207
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-03-23 21:48:58 +0800

    util: ExcelColumn

--- util/commons_util/fundamentals/data_structs.py
+++ util/commons_util/fundamentals/data_structs.py
@@ -11,3 +11,32 @@ def argsort(A, f=None):
     if f == None:
         f = lambda k: A[k]
     return sorted(range(n), key=f)
+
+
+class ExcelColumn(object):
+    def __init__(self):
+        self.cur = None
+        self.restore()
+
+    def restore(self):
+        self.cur = []
+
+    def _plus(self, cur, idx):
+        if idx>=len(cur):
+            cur.append('a')
+        elif cur[idx]<'z':
+            cur[idx] = chr(ord(cur[idx])+1)
+        else:
+            cur[idx] = 'a'
+            self._plus(cur, idx+1)
+
+    def columns(self, n):
+        """
+        generator for excel columns
+        :param n: decimal count for excel col
+        :return:
+        """
+        self.restore()
+        for i in xrange(n):
+            self._plus(self.cur, 0)
+            yield ''.join(reversed(self.cur))
--- util/tests/fundamentals/test_data_structs.py
+++ util/tests/fundamentals/test_data_structs.py
@@ -8,3 +8,7 @@ class TestDataStructs(TestCase):
         A = [3, 2, 1]
         ret = argsort(A)
         self.assertEqual(ret, [2, 1, 0])
+
+    def test_excel_column(self):
+        col = ExcelColumn()
+        self.assertEqual(list(col.columns(800))[-1], 'adt')
